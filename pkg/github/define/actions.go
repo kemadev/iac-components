51c064a96640179d529315246536886608e69ffd
kema-dev <github@kema.dev>
2025-03-14 14:44:18 +0100

    up

--- pkg/github/define/actions.go
+++ pkg/github/define/actions.go
@@ -4,7 +4,7 @@ const (
 	// GitDefaultCommitMessage is the default commit message when committing changes to the repository using pulumi
 	GitDefaultCommitMessage = "chore(repo-sync): update repository files"
 	// GitCommiterName is the name of the git commiter when committing changes to the repository using pulumi
-	GitCommiterName = "pulumi[bot]"
+	GitCommiterName = "github-actions[bot]"
 	// GitCommiterEmail is the email of the git commiter when committing changes to the repository using pulumi
-	GitCommiterEmail = "kemadev+pulumi[bot]@users.noreply.github.com"
+	GitCommiterEmail = "kemadev+github-actions[bot]@users.noreply.github.com"
 )
